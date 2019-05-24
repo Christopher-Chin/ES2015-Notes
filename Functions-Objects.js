f81c9f081fcbddf63e20a88ac59961589c5dd20b
Christopher Chin <christopher.phil.chin@gmail.com>
2019-05-24 22:38:43 +1000

    Update Functions-Objects.js

--- Functions-Objects.js
+++ Functions-Objects.js
@@ -42,14 +42,21 @@ divide();
   console.log("12 divided by 0.75 is ", result);
 }())
 
-function findBiggestFraction(){
-  a>b ? console.log("a: ",a) : console.log("b: ",b);
+
+
+function findBiggestFraction(a,b) {
+    var result;
+    a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
+    return result;
 }
 
-var a = 3/4;
-var b = 5/7;
+var firstFraction = 3/4;
+var secondFraction = 5/7;
 
-findBiggestFraction();
+var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 
+console.log("First fraction result: ", firstFraction);
+console.log("Second fraction result: ", secondFraction);
+console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
 
   
